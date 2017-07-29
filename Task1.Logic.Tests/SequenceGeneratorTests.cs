e113dbc4f2be652bba522181c16be0f422bd6c04
zmitr-gen <zmitr-gen@yandex.ru>
2017-07-29 14:33:59 +0300

    Was added logic of generating sequence of Fibonacci numbers. Was covered by unit tests.

---
+++ Task1.Logic.Tests/SequenceGeneratorTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static Task1.Logic.SequenceGenerator;
+
+namespace Task1.Logic.Tests
+{
+    [TestFixture]
+    public class SequenceGeneratorTests
+    {
+        #region Good script
+
+        [Test]
+        public void GenerateFibonacciNumbers_BoundIs1_ReturnsSequenceOfTwoElements()
+        {
+            IEnumerable<int> expectedRes = new int[] { 1, 1 };
+
+            IEnumerable<int> res = GenerateFibonacciNumbers(1);
+
+            Assert.AreEqual(expectedRes, res);
+        }
+
+        [Test]
+        public void GenerateFibonacciNumbers_BoundIs5_ReturnsSequenceOfFiveElements()
+        {
+            IEnumerable<int> expectedRes = new int[] { 1, 1, 2, 3, 5 };
+
+            IEnumerable<int> res = Logic.SequenceGenerator.GenerateFibonacciNumbers(5);
+
+            Assert.AreEqual(expectedRes, res);
+        }
+
+        [Test]
+        public void GenerateFibonacciNumbers_BoundIs7_ReturnsSequenceOfFiveElements()
+        {
+            IEnumerable<int> expectedRes = new int[] { 1, 1, 2, 3, 5 };
+
+            IEnumerable<int> res = Logic.SequenceGenerator.GenerateFibonacciNumbers(7);
+
+            Assert.AreEqual(expectedRes, res);
+        }
+
+        #endregion
+
+
+        #region Exceptions
+
+        [Test]
+        public void GenerateFibonacciNumbers_BoundIsLessThanZero_ThrowsArgumentException()
+        {
+            Assert.Catch<ArgumentException>(() =>
+            {
+                foreach (var n in GenerateFibonacciNumbers(-3))//HERE
+                {
+                    int b = n;
+                }
+            });
+        }
+
+        [Test]
+        public void GenerateFibonacciNumbers_BoundIsZero_ThrowsArgumentException()
+        {
+            IEnumerable<int> res;
+            
+            res = GenerateFibonacciNumbers(0);//NO EXCEPTION!
+
+            Assert.Catch<ArgumentException>(() =>
+            {
+                foreach (var n in res)//HERE
+                {
+                    int b = n;
+                }
+            });
+
+        }
+
+        #endregion
+    }
+}
---
+++ Task1.Logic/SequenceGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Logic
+{
+    /// <summary>
+    /// Generate various sequences of numbers.
+    /// </summary>
+    public static class SequenceGenerator
+    {
+        /// <summary>
+        /// Generate sequence of fibonacci numbers up to a certain limit. 
+        /// </summary>
+        /// <param name="bound"> The boundary to which array will be generated. </param>
+        /// <returns> Sequence of int fibonacci numbers. </returns>
+        public static IEnumerable<int> GenerateFibonacciNumbers(int bound)
+        {
+            if (bound < 1)
+                throw new ArgumentException("The bound must be more than zero.");
+
+            int prev = 0, curr = 1;
+           
+            while (curr <= bound)
+            {
+
+                yield return curr;
+
+                int temp = curr;
+                curr = prev + curr;
+                prev = temp;
+            }
+        }
+    }
+}
